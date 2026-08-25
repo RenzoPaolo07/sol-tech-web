0243b347a9f799b4abd7e98efdd6b4e00e6f7dac
RenzoPaolo07 <60995421@continental.edu.pe>
2026-08-25 00:40:42 -0500

    Version con medios de comunicacion 3.0

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'soltech-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
+  'https://unpkg.com/aos@2.3.1/dist/aos.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
